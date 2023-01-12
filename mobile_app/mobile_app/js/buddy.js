9283dda0fee45cb36c726c7c91d721e6f9ba9ff5
Andrija Hanga <a.hanga@student.fontys.nl>
2023-01-12 17:48:34 +0100

    ChangeText function and groceries page added

--- mobile_app/mobile_app/js/buddy.js
+++ mobile_app/mobile_app/js/buddy.js
@@ -2,35 +2,33 @@ import { GLTFLoader } from "./GLTFLoader.js";
 
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(
-    125,
-    window.innerWidth / window.innerHeight,
-    0.01,
-    1000,
+  125,
+  window.innerWidth / window.innerHeight,
+  0.01,
+  1000
 );
 
 let renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 const loader = new GLTFLoader();
 
 var obj;
 
-loader.load("./js/tiger/scene.gltf", function(gltf){
-    obj = gltf.scene;
-    scene.add(gltf.scene);
+loader.load("./js/tiger/scene.gltf", function (gltf) {
+  obj = gltf.scene;
+  scene.add(gltf.scene);
 });
-scene.background = new THREE.Color(0xffffff)
-let light = new THREE.HemisphereLight(0xffffff, 0x000000, 2)
+scene.background = new THREE.Color(0xffffff);
+let light = new THREE.HemisphereLight(0xffffff, 0x000000, 2);
 scene.add(light);
-camera.position.set(0,2,2);
+camera.position.set(0, 2, 2);
 
-function animate(){
-    requestAnimationFrame(animate);
-    obj.rotation.y += 0.0005;
-    renderer.render(scene,camera);
-};
+function animate() {
+  requestAnimationFrame(animate);
+  //obj.rotation.y += 0.0005;
+  renderer.render(scene, camera);
+}
 
 animate();
-
-
---
+++ mobile_app/mobile_app/js/buddyClick.js
@@ -0,0 +1,25 @@
+function changeText() {
+  var text = document.getElementById("tiger");
+  var item1 = document.getElementById("item1");
+  var item2 = document.getElementById("item2");
+  var item3 = document.getElementById("item3");
+  var item4 = document.getElementById("item4");
+  var h1 = document.getElementById("tigerSays");
+  text.style.cursor = "pointer";
+  const items = [item1, item2, item3, item4];
+  let random = Math.floor(Math.random() * 20) + 1;
+
+  text.onclick = function () {
+    h1.textContent = "Please select the item from list you need help with";
+
+    for (let i = 0; i < items.length; i++) {
+      items[i].style.cursor = "pointer";
+      items[i].onclick = function () {
+        h1.textContent = "Please go to section " + random;
+        setTimeout(() => {
+          window.location.href = "./buddyToScreenPage.html";
+        }, 5000);
+      };
+    }
+  };
+}
